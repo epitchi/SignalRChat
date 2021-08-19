e364317bd11ef791bbe83443172a6cdd92931f0e
epitchi <thienvanlea1@gmail.com>
2021-08-19 15:53:41 +0700

    Fix bug cant send. Completed.

--- SignalRChat/wwwroot/js/chat.js
+++ SignalRChat/wwwroot/js/chat.js
@@ -3,19 +3,19 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 //Disable send button until connection is established
-document.getElementById("sendMessage").disable = true;
+document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
-    document.getElementById("messageList").appendChild(li);
-    // We can assign user-supplied string to an element's textContent 
-    // because it is not interpreted as markup. If you're assigning in any other way,
-    // you should be aware of possible script injection concerns.
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
     li.textContent = `${user} says ${message}`;
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disable = false;
+    document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
@@ -27,4 +27,4 @@ document.getElementById("sendButton").addEventListener("click", function (event)
         return console.error(err.toString());
     });
     event.preventDefault();
-})
+});
